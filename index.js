a5c0d95bb3c44f978dc8c323b60bae92414c4f68
DeyvetWalef <deyvet.martins@edu.ufes.br>
2022-06-01 21:44:36 -0300

    up

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ app.use(express.static(path.join(__dirname, "public")));
 const pokedex = [
   {
     id: 1,
-    nome: "Charmander",
+    nome: "CHARMANDER",
     tipo: "Fogo",
     imagem:
       "https://www.nicepng.com/png/detail/278-2789894_pokemon-charmander-vector.png",
     descricao:
       "Tem preferência por coisas quentes. Quando chove, diz-se que o vapor jorra da ponta de sua cauda.",
-    altura: "0.6m",
-    peso: "8.5kg",
+    altura: "0.6",
+    peso: "8.5",
     categoria: "Lagarto",
     habilidade: "Cuspir Fogo",
   },
@@ -27,8 +27,8 @@ const pokedex = [
       "https://www.nicepng.com/png/detail/99-991833_bulbasaur-png-pic-bulbasaur-png.png",
     descricao:
       "Há uma semente de planta nas costas desde o dia em que este Pokémon nasce. A semente cresce lentamente.",
-    altura: "0.7m",
-    peso: "6.9kg",
+    altura: "0.7",
+    peso: "6.9",
     categoria: "Semente",
     habilidade: "Superar",
   },
@@ -40,8 +40,8 @@ const pokedex = [
       "https://www.nicepng.com/png/detail/147-1474808_squirtle-free-png-image-pokemon-turtle.png",
     descricao:
       "Quando retrai seu longo pescoço em sua concha, esguicha água com força vigorosa.",
-    altura: "0.5m",
-    peso: "9.0kg",
+    altura: "0.5",
+    peso: "9.0",
     categoria: "Tartaruga",
     habilidade: "Corrente de água",
   },
@@ -51,6 +51,10 @@ const pokedex = [
 app.get("/", (req, res) => {
   res.render("index", { pokedex });
 });
+
+app.post("/add", (req, res) =>){
+    
+}
 
 app.listen(3000, () =>
   console.log("Servidor rodando em https://localhost :3000")
